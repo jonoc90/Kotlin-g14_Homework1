dce4034115f7ce80e27183e3b58f99cc4cbbb4bf
Jonathan <jonoc90@gmail.com>
2020-02-06 14:42:09 -0500

    This is First Git

---
+++ app/src/main/java/com/jonacorp/myapplication/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.jonacorp.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startUI()
+
+    }
+
+    fun startUI(){
+        //Start code method
+        btnShowToast.setOnClickListener {
+            showLog("Toast button clicked") // -> Only Developers
+            Toast.makeText(this, "Toast clicked",Toast.LENGTH_LONG).show()
+        }
+    }
+
+/*
+* In this secttion we're gonna add complementary functions for the app...
+* */
+   //-------- Logs Method for Developers --------
+    fun showLog(message:String){
+        if (BuildConfig.DEBUG){
+            Log.v("CONSOLE", message)
+        }
+    }   //----------- END Log -------------------
+
+
+
+
+
+}
